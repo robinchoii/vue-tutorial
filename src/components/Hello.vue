eaacb064adee9f7ea8e90a8b32f0db4cf6a9e0cf
robinchoii <robinchoii@gmail.com>
2017-10-07 15:32:17 -0700

    data binding

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <hr />
+    <div>
+      <ul class='names'>
+        <li v-for="user in users">
+          {{user.firstname}} {{user.lastname}}
+        </li>
+      </ul>
+    </div>
+    <hr />
+    <div>
+      <input type="text" v-model="input_val">
+    </div>
+    <div>
+      Input Value : <span v-text='input_val'></span>
+    </div>
+    <hr />
+    <div>
+      <button class='btn btn-primary' v-on:click='count++'>You've clicked this button {{count}} times!</button>
+    </div>
   </div>
 </template>
 
@@ -25,7 +28,14 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      users: [
+              {firstname: "Robin", lastname: "Choi"},
+              {firstname: "Sarah", lastname: "Pak"},
+              {firstname: "Hannah", lastname: "Lee"}
+            ],
+      input_val: '',
+      count: 0
     }
   }
 }
@@ -43,11 +53,15 @@ ul {
 }
 
 li {
-  display: inline-block;
+  /*display: inline-block;*/
   margin: 0 10px;
 }
 
 a {
   color: #42b983;
+}
+
+.names {
+  color: red;
 }
 </style>
